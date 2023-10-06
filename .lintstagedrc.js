8643a67fcc1f667ceed517dc87b211f7c68b56b0
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-06 19:15:03 +0100

    build: :construction: fix to format

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,5 @@ module.exports = {
   '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md}': [
     'nx affected:lint --uncommitted --fix true',
   ],
-  '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
-    'nx format:write --uncommitted',
-  ],
+  '*.{ts,js,html,json,scss,css,md,yaml,yml}': ['nx format:write --uncommitted'],
 };
