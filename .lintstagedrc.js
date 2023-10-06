83a5e2350571fe102236586c5e009de2b29a189d
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-06 19:47:30 +0100

    chore: :art: last fix to format for now

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md}': [
     'nx affected:lint --uncommitted --fix true'
   ],
-  '*.{ts,js,html,json,scss,css,md,yaml,yml}': [
-    'nx format:write --uncommitted --fix true'
+  '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
+    'nx format:write --uncommitted'
   ]
 };
