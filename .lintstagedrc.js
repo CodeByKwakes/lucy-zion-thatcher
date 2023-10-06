300f22287e45e18c884d1694c6f80f0472b64c9e
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-06 20:16:28 +0100

    feat: :sparkles: core data access lib created

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md}': [
-    'nx affected:lint --uncommitted --fix true'
-  ],
+  '{apps,libs,src}/**/*.{ts,js}': ['nx affected:lint --uncommitted --fix true'],
   '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
     'nx format:write --uncommitted'
   ]
