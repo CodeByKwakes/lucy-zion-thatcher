908589423be35a51abb0ca50eaa94af19b4ef277
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-06 19:40:41 +0100

    chore: :art: prettier code extended

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md}': [
-    'nx affected:lint --uncommitted --fix true',
+    'nx affected:lint --uncommitted --fix true'
   ],
-  '*.{ts,js,html,json,scss,css,md,yaml,yml}': ['nx format:write --uncommitted'],
+  '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
+    'nx format:write --uncommitted --fix true'
+  ],
 };
