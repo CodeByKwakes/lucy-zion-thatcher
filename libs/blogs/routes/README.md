826a7c28d151b0fe20017ca5aed54599d79571c0
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-08 19:23:18 +0100

    feat: :sparkles: blog feature and route created

---
+++ libs/blogs/routes/README.md
@@ -0,0 +1,7 @@
+# blogs-routes
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test blogs-routes` to execute the unit tests.
---
+++ libs/blogs/routes/src/lib/lib.routes.ts
@@ -0,0 +1,5 @@
+import { Route } from '@angular/router';
+
+export const routesRoutes: Route[] = [
+  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+];
