80a9fceab4e3e871ac20392990fea4d7e056473c
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-09 01:26:09 +0100

    chore: :construction: more format clean up

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
   rules: {
-    'scope-enum': [2, 'always', ['web-app', 'blogs', 'core','pages', 'shared']],
-  },
+    'scope-enum': [2, 'always', ['web-app', 'blogs', 'core', 'pages', 'shared']]
+  }
 };
