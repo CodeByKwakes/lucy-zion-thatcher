4fc0561fc311e88576b1d9aac3c53b3835c062bf
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-09 01:44:42 +0100

    chore: :recycle: one more try

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '{apps,libs,src}/**/*.{ts,js}': ['nx affected:lint --uncommitted --fix true'],
-  '{apps,libs,src}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
-    'nx format:write'
+  '*.{ts,js,html,json,scss,css,md,yaml,yml}': [
+    'nx format:write --uncommitted --fix true'
   ]
 };
