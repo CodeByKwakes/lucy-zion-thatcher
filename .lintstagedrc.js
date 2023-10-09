ab83d1cc2c39457ff299a572802e4c013d29aa01
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-09 01:50:08 +0100

    chore: :construction: another try

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  '{apps,libs,src}/**/*.{ts,js}': ['nx affected:lint --uncommitted --fix true'],
-  '*.{ts,js,html,json,scss,css,md,yaml,yml}': [
-    'nx format:write --uncommitted --fix true'
+  '**/*.{ts,js}': [
+    'nx affected:lint --uncommitted --fix true',
+    'git add' // Add the changes to the staging area
+  ],
+  '**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
+    'nx format:write --uncommitted --fix true',
+    'git add' // Add the changes to the staging area
   ]
 };
