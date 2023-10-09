c02e52b4dee3c37be9a3e0f37625854425af099c
Kwakes Prempeh <kwakesproject@gmail.com>
2023-10-09 01:19:43 +0100

    chore: :construction: formating fixes for angular

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -50,25 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   /**
-   * Toggle mobile nav dropdowns
-   */
-  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
-
-  navDropdowns.forEach((el) => {
-    el.addEventListener('click', function (event) {
-      if (document.querySelector('.mobile-nav-active')) {
-        event.preventDefault();
-        this.classList.toggle('active');
-        this.nextElementSibling.classList.toggle('dropdown-active');
-
-        let dropDownIndicator = this.querySelector('.dropdown-indicator');
-        dropDownIndicator.classList.toggle('bi-chevron-up');
-        dropDownIndicator.classList.toggle('bi-chevron-down');
-      }
-    });
-  });
-
-  /**
    * Scroll top button
    */
   const scrollTop = document.querySelector('.scroll-top');
@@ -84,127 +65,9 @@ document.addEventListener('DOMContentLoaded', () => {
       'click',
       window.scrollTo({
         top: 0,
-        behavior: 'smooth',
+        behavior: 'smooth'
       })
     );
-  }
-
-  /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox',
-  });
-
-  /**
-   * Init swiper slider with 1 slide at once in desktop view
-   */
-  new Swiper('.slides-1', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  /**
-   * Init swiper slider with 3 slides at once in desktop view
-   */
-  new Swiper('.slides-3', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
-
-      1200: {
-        slidesPerView: 3,
-      },
-    },
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  let portfolionIsotope = document.querySelector('.portfolio-isotope');
-
-  if (portfolionIsotope) {
-    let portfolioFilter = portfolionIsotope.getAttribute(
-      'data-portfolio-filter'
-    )
-      ? portfolionIsotope.getAttribute('data-portfolio-filter')
-      : '*';
-    let portfolioLayout = portfolionIsotope.getAttribute(
-      'data-portfolio-layout'
-    )
-      ? portfolionIsotope.getAttribute('data-portfolio-layout')
-      : 'masonry';
-    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort')
-      ? portfolionIsotope.getAttribute('data-portfolio-sort')
-      : 'original-order';
-
-    window.addEventListener('load', () => {
-      let portfolioIsotope = new Isotope(
-        document.querySelector('.portfolio-container'),
-        {
-          itemSelector: '.portfolio-item',
-          layoutMode: portfolioLayout,
-          filter: portfolioFilter,
-          sortBy: portfolioSort,
-        }
-      );
-
-      let menuFilters = document.querySelectorAll(
-        '.portfolio-isotope .portfolio-flters li'
-      );
-      menuFilters.forEach(function (el) {
-        el.addEventListener(
-          'click',
-          function () {
-            document
-              .querySelector(
-                '.portfolio-isotope .portfolio-flters .filter-active'
-              )
-              .classList.remove('filter-active');
-            this.classList.add('filter-active');
-            portfolioIsotope.arrange({
-              filter: this.getAttribute('data-filter'),
-            });
-            if (typeof aos_init === 'function') {
-              aos_init();
-            }
-          },
-          false
-        );
-      });
-    });
   }
 
   /**
@@ -215,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
       duration: 800,
       easing: 'slide',
       once: true,
-      mirror: false,
+      mirror: false
     });
   }
   window.addEventListener('load', () => {
