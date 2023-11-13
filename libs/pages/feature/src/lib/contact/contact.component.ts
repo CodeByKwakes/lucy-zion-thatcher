2750075cef62c89515f1e66555aa9cbdf4c62abc
Kwakes Prempeh <kwakesproject@gmail.com>
2023-11-13 21:46:32 +0000

    fix: :pencil2: fix formatting issues in HTML files

--- libs/pages/feature/src/lib/contact/contact.component.ts
+++ libs/pages/feature/src/lib/contact/contact.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from '@lzt/shared/ui-components';
+import { usePageFeature } from '@lzt/pages/data-access';
+import { GlobalPage } from '@lzt/shared/models';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lib-contact',
   standalone: true,
-  imports: [CommonModule, BreadcrumbsComponent],
+  imports: [CommonModule, BreadcrumbsComponent, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {}
+export class ContactComponent {
+  readonly #pageStore = usePageFeature();
+  #formBuilder = inject(FormBuilder);
+
+  readonly $globalPage = this.#pageStore.$getPageBySlug(
+    'global'
+  ) as Signal<GlobalPage>;
+
+  readonly contactForm = this.#formBuilder.group({
+    name: ['', Validators.required],
+    email: [''],
+    subject: [''],
+    message: ['']
+  });
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
