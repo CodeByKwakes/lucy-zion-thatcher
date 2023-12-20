b993e2db241c31e253953f2ab1a575533aca2ea2
Kwakes Prempeh <codebykwakes@gmail.com>
2023-12-20 16:46:48 +0000

    refactor: update stylelint configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,5 +4,6 @@ module.exports = {
   ],
   '{src,apps,libs,tools}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
     'nx format:write --uncommitted --fix true'
-  ]
+  ],
+  '{src,apps,libs,tools}/**/*.{css,less,sass,scss,sss}': ['npx stylelint --fix']
 };
