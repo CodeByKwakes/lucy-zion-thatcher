9fccfc87508842a005739eb53ea726fae5320279
Kwakes Prempeh <codebykwakes@gmail.com>
2024-01-01 23:28:51 +0000

    feat: add in-memory scrolling to app router for scroll restoration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import {
   provideRouter,
-  withEnabledBlockingInitialNavigation
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling
 } from '@angular/router';
 import { provideCoreStore } from '@lzt/core/data-access';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideRouter(
+      appRoutes,
+      withEnabledBlockingInitialNavigation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+    ),
     provideCoreStore()
   ]
 };
