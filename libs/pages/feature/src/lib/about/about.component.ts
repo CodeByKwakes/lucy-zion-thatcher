01572fa88747b8f7853d1e04f45630bf0e803b5e
Kwakes Prempeh <codebykwakes@gmail.com>
2024-01-06 04:08:39 +0000

    feat: introduce dynamic page content component

--- libs/pages/feature/src/lib/about/about.component.ts
+++ libs/pages/feature/src/lib/about/about.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Signal, inject } from '@angular/core';
 import { PageStore } from '@lzt/pages/data-access';
+import { PageContentComponent } from '@lzt/pages/ui';
 import { AboutPage } from '@lzt/shared/models';
 import { BreadcrumbsComponent } from '@lzt/shared/ui-components';
 import { GetAssetPipe } from '@lzt/shared/utils';
@@ -8,7 +9,12 @@ import { GetAssetPipe } from '@lzt/shared/utils';
 @Component({
   selector: 'lib-about',
   standalone: true,
-  imports: [CommonModule, BreadcrumbsComponent, GetAssetPipe],
+  imports: [
+    CommonModule,
+    BreadcrumbsComponent,
+    GetAssetPipe,
+    PageContentComponent
+  ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
--- libs/pages/feature/src/lib/speaker/speaker.component.ts
+++ libs/pages/feature/src/lib/speaker/speaker.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Signal, inject } from '@angular/core';
 import { PageStore } from '@lzt/pages/data-access';
+import { PageContentComponent } from '@lzt/pages/ui';
 import { SpeakerPage } from '@lzt/shared/models';
 import { BreadcrumbsComponent } from '@lzt/shared/ui-components';
-import { GetAssetPipe } from '@lzt/shared/utils';
 
 @Component({
   selector: 'lib-speaker',
   standalone: true,
-  imports: [CommonModule, BreadcrumbsComponent, GetAssetPipe],
+  imports: [CommonModule, BreadcrumbsComponent, PageContentComponent],
   templateUrl: './speaker.component.html',
   styleUrls: ['./speaker.component.scss']
 })
