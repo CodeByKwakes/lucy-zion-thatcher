f09e8899adb3ac6182ee91a3b87bfbee58b699db
Kwakes Prempeh <codebykwakes@gmail.com>
2024-04-19 22:11:06 +0100

    feat: add descriptive titles to pages for search engine optimization

--- libs/pages/feature/src/lib/lib.routes.ts
+++ libs/pages/feature/src/lib/lib.routes.ts
@@ -7,21 +7,25 @@ export const pagesRoutes: Route[] = [
       { path: '', pathMatch: 'full', redirectTo: 'home' },
       {
         path: 'home',
+        title: 'Home | Lucy Zion Thatcher',
         loadComponent: async () =>
           (await import('./home/home.component')).HomeComponent
       },
       {
         path: 'about',
+        title: 'About Lucy | Lucy Zion Thatcher',
         loadComponent: async () =>
           (await import('./about/about.component')).AboutComponent
       },
       {
         path: 'speaker',
+        title: 'Lucy - The Speaker | Lucy Zion Thatcher',
         loadComponent: async () =>
           (await import('./speaker/speaker.component')).SpeakerComponent
       },
       {
         path: 'contact',
+        title: 'Contact Lucy | Lucy Zion Thatcher',
         loadComponent: async () =>
           (await import('./contact/contact.component')).ContactComponent
       }
