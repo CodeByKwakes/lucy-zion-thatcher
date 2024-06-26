e34ae0b7ff4771b8c498d4d6a655b136f5710d76
Kwakes Prempeh <codebykwakes@gmail.com>
2024-06-26 23:33:32 +0100

    refactor: update import paths and file paths for pages UI module

--- modules/pages/ui/jest.config.ts
+++ modules/pages/ui/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'pages-ui',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../coverage/libs/pages/ui',
+  coverageDirectory: '../../../coverage/modules/pages/ui',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
