b8d96b22fe77538127af539bc361fd9789341b19
Kwakes Prempeh <codebykwakes@gmail.com>
2024-07-28 00:22:17 +0100

    feat: add lazy loading for images in home component
    - Added lazy loading functionality for images in the home component using the Swiper library.
    - Updated the home component to implement the AfterViewInit lifecycle hook.

--- modules/pages/feature/src/lib/home/home.component.ts
+++ modules/pages/feature/src/lib/home/home.component.ts
@@ -1,21 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component, Signal, inject } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  ElementRef,
+  Signal,
+  ViewChild,
+  inject,
+  AfterViewInit
+} from '@angular/core';
 import { useCoreStore } from '@lzt/core/api';
 import { PageStore } from '@lzt/pages/domain';
 import { HomePage } from '@lzt/shared/models';
 import { GetAssetPipe, SafeHtmlPipe } from '@lzt/shared/utils';
+import { SwiperContainer } from 'swiper/element';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   standalone: true,
   imports: [CommonModule, GetAssetPipe, SafeHtmlPipe],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('swiper') swiper!: ElementRef<SwiperContainer>;
+
   readonly #coreStore = useCoreStore();
   readonly #pageStore = inject(PageStore);
 
   readonly currentPage = this.#pageStore.selectCurrentPage as Signal<HomePage>;
+
+  ngAfterViewInit() {
+    const swiperParams = {
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+
+        1200: {
+          slidesPerView: 3
+        }
+      }
+    } as SwiperOptions;
+
+    Object.assign(this.swiper.nativeElement, swiperParams);
+    this.swiper.nativeElement.initialize();
+  }
 
   onRouteToPage(page: string): void {
     this.#coreStore.routeTo([page]);
