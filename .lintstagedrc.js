c2e31b5a68308c68556fc56cb1613519f36dcd18
Kwakes Prempeh <codebykwakes@gmail.com>
2024-10-12 21:10:18 +0100

    refactor(blogs): update blog detail and list components

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   '{src,apps,modules}/**/*.{ts,js}': [
     'nx affected:lint --uncommitted --fix true'
-  ],
-  '{src,apps,modules}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
-    'nx format:write --uncommitted --fix true'
   ]
+  // '{src,apps,modules}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
+  //   'nx format:write --uncommitted --fix true'
+  // ]
 };
