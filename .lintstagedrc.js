7136efbab05baaf2390f83addce6cdeb9aa89c34
Kwakes Prempeh <codebykwakes@gmail.com>
2024-10-28 23:55:40 +0000

    refactor: update linting and formatting rules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,14 @@
 module.exports = {
+  '{src,apps,modules}/**/*.{json,md,html}': [
+    'nx format:write --uncommitted --fix true'
+  ],
   '{src,apps,modules}/**/*.{ts,js}': [
+    'nx format:write --uncommitted --fix true',
     'nx affected:lint --uncommitted --fix true'
+  ],
+  '{src,apps,modules}/**/*.{scss,css}': [
+    'nx format:write --uncommitted --fix true',
+    'stylelint --fix'
   ]
   // '{src,apps,modules}/**/*.{ts,js,html,json,scss,css,md,yaml,yml}': [
   //   'nx format:write --uncommitted --fix true'
